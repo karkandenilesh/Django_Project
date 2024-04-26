5891b261fec41b6525e665b4ddf38f4a62a6dc10
Nilesh Karkande <karkandenilesh10@gmail.com>
2024-04-26 11:24:34 +0530

    changes made in setting file

--- organization/test/test_urls.py
+++ organization/test/test_urls.py
@@ -1,10 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+from rest_framework import generics
 from organization.views import upload_csv, FilterOrganizations, user_list, add_user, delete_user
 
 
-class TestUrls(TestCase):
+class UrlTests(TestCase):
+    def setUp(self):
+        # Initialize the Django test client
+        self.client = Client()
 
-    def test_list_url_is_resolved(self):
-        url = reverse('user_list')
-        self.assertEqual(resolve(url).func, user_list)
+    def test_url_is_accessible(self):
+        url = reverse('upload_csv')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_url_resolves_to_view(self):
+        # Define the URL pattern you want to test
+        url = reverse('filter_organizations_api')
+
+        # Use resolve() to match the URL to a view function
+        resolver_match = resolve(url)
+
+        # Assert that the resolved view function is an instance of generics.ListAPIView
+        self.assertIsInstance(resolver_match.func.cls, generics.ListAPIView)
+
+        # Alternatively, you can directly check if the resolved view function is FilterOrganizations
+        self.assertEqual(resolver_match.func.cls, FilterOrganizations)
+
